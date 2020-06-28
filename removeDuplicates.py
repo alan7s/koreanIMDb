0bf51c4f8e0a86263a84bcb3a7a141ad30f57afb
alan7s <alan7@outlook.com>
2020-06-27 23:02:43 -0300

    Update removeDuplicates.py

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -2,4 +2,4 @@ import pandas as pd
 df = pd.read_csv("koreanTitles.csv")
 print(df.drop_duplicates(subset=['ID']).shape)
 dx = df.drop_duplicates(subset=['ID'])
-dx.to_csv('semDuplicados.csv', index=False, encoding='utf-8')
+dx.to_csv('koreanTitles.csv', index=False, encoding='utf-8')
